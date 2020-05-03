e1f2295da0db8f9cad4e65379de48770498aabbd
Sean Blumenfeld <seanbl@me.com>
2020-05-03 13:46:56 +0100

    Add test to ruuvitag watcher service

--- services/ruuvitags/watch_sensor_events.py
+++ services/ruuvitags/watch_sensor_events.py
@@ -8,14 +8,14 @@ from simple_ruuvitag import RuuviTagClient
 
 logger = structlog.getLogger()
 
-events_api = 'http://localhost:8000/events/'  # TODO: parameterize? Move to settings?
+EVENTS_API = 'http://localhost:8000/events/'  # TODO: parameterize? Move to settings?
 SENSOR_MACS = ['FE:F6:ED:CF:47:F3']
 
 
 def save_sensor_event(mac, data):
-    logger.debug(f'Received sensor data for {mac}')
-    response = requests.post(events_api, json={'data': json.dumps(data, cls=encoders.JSONEncoder)})
-    logger.debug(f'Response: {response.status_code}')
+    logger.info(f'Received sensor data for {mac}')
+    response = requests.post(EVENTS_API, json={'data': json.dumps(data, cls=encoders.JSONEncoder)})
+    logger.info(f'Response: {response.status_code}')
 
 
 if __name__ == '__main__':
