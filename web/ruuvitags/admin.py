4ac0a89b2fff385de5f468ae5b239354b38e0d38
Sean Blumenfeld <seanbl@me.com>
2020-05-26 11:23:27 +0100

    Refactor ruuvitag gateway service

--- web/ruuvitags/admin.py
+++ web/ruuvitags/admin.py
@@ -15,16 +15,19 @@ class EventAdmin(admin.ModelAdmin):
         'acceleration_y', 'acceleration_z', 'tx_power', 'battery', 'movement_counter',
         'measurement_sequence_number', 'mac', 'created', 'updated'
     ]
-    list_display = ['id', 'mac_address', 'data_format', 'temperature', 'created']
+    list_display = ['sensor_name', 'mac_address', 'data_format', 'temperature', 'created']
     list_filter = ['mac']
     ordering = ['-created']
+
+    def sensor_name(self, obj):
+        return obj.sensor.name
 
 
 @admin.register(Sensor)
 class SensorAdmin(admin.ModelAdmin):
     fields = ['name', 'mac_address', 'created', 'updated']
     readonly_fields = ['id', 'mac_address', 'created', 'updated']
-    list_display = ['id', 'mac_address', 'created', 'updated']
+    list_display = ['name', 'mac_address', 'created', 'updated']
 
     def __str__(self):
         return self.name
