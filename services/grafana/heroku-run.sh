7859d053ac65e63ca1ace6db82850e4bf3663814
sean.blumenfeld <seanbl@me.com>
2022-08-11 20:10:58 +0100

    grafana ini updates

--- services/grafana/heroku-run.sh
+++ services/grafana/heroku-run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i -e 's|<HTTP_PORT>|'$PORT'|' /etc/grafana/grafana.ini
-sed -i -e 's|<DATABASE_URL>|'$DATABASE_URL'|' /etc/grafana/grafana.ini
-sed -i -e 's|<ADMIN_USER>|'$ADMIN_USER'|' /etc/grafana/grafana.ini
-sed -i -e 's|<ADMIN_PASSWORD>|'$ADMIN_PASSWORD'|' /etc/grafana/grafana.ini
+sed -e 's|<HTTP_PORT>|'${PORT}'|' -i /etc/grafana/grafana.ini
+sed -e 's|<DATABASE_URL>|'${DATABASE_URL}'|' -i /etc/grafana/grafana.ini
+sed -e 's|<ADMIN_USER>|'${ADMIN_USER}'|' -i /etc/grafana/grafana.ini
+sed -e 's|<ADMIN_PASSWORD>|'${ADMIN_PASSWORD}'|' -i /etc/grafana/grafana.ini
 
 /run.sh
